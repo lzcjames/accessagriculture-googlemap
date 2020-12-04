342e452245f88e1d49d770a47734da4714c6fba9
lzcjames <451845431.cl@gmail.com>
2020-12-05 00:07:44 +0800

    use json data

--- page.js
+++ page.js
@@ -4,8 +4,7 @@ function initMap() {
     zoom: 3,
     center: { lat: -28.024, lng: 140.887 },
   });
-  // Create an array of alphabetical characters used to label the markers.
-  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
   // Add some markers to the map.
   // Note: The code uses the JavaScript Array.prototype.map() method to
   // create an array of markers based on a given "countries" array.
@@ -13,12 +12,17 @@ function initMap() {
   markers = countries.map((country, i) => {
     let marker = new google.maps.Marker({
       position: country, // it detects automatically the values of keys which named "lat" and "lng"
-      label: labels[i % labels.length],
+      label: country["country"],
       map: map   
+    });
+    // Retrieve languages and links
+    let info='';
+    country["languages"].map((language) =>{
+      info+=`<button><a href=${language["link"]}>${language["name"]}</a></button> `;   
     });
     // Create an info window
     const infowindow = new google.maps.InfoWindow({
-      content: country.info
+      content: info
     });
     //Add a click event to each marker
     marker.addListener("click", () => {
@@ -34,19 +38,53 @@ function initMap() {
 let infowindows=[];
 let markers;
 
-const countries = [
-  { lat: -31.56391, lng: 147.154312, info: "marker 1" },
-  { lat: -33.718234, lng: 150.363181, info: "marker 2" },
-  { lat: -33.727111, lng: 150.371124, info: "marker 3" },
-  { lat: -33.848588, lng: 151.209834, info: "marker 4" },
-  { lat: -33.851702, lng: 151.216968, info: "marker 5" },
-  { lat: -34.671264, lng: 150.863657, info: "marker 6" },
-  { lat: -35.304724, lng: 148.662905, info: "marker 7" },
-  { lat: -36.817685, lng: 175.699196, info: "marker 8" },
-];
 
 document.getElementById("test").addEventListener("click", function() {
   for (var i=0;i<infowindows.length;i++) {
     infowindows[i].open(map, markers[i]);
   }
 });
+
+
+const countries = [
+  {
+    "country": "Vanuatu",
+    "lat": 15.3767,
+    "lng": 166.9592,
+    "description": "Vanuatu is a South Pacific Ocean nation made up of roughly 80 islands that stretch 1,300 kilometers.",
+    "languages": [
+      {
+        "name": "English",
+        "link": "http://example.com/en"
+      },
+      {
+        "name": "French",
+        "link": "http://example.com/fr"
+      }
+    ]
+  },
+  {
+    "country": "Fiji",
+    "lat": -16.578193,
+    "lng": 179.414413,
+    "description": "A country in the South Pacific, is an archipelago of more than 300 islands.",
+    "languages": [
+      {
+        "name": "English",
+        "link": "http://example.com/en"
+      }
+    ]
+  },
+  {
+    "country": "Panama",
+    "lat": 8.537981,
+    "lng": -80.782127,
+    "description": "A country on the isthmus linking Central and South America.",
+    "languages": [
+      {
+        "name": "Spanish",
+        "link": "http://example.com/es"
+      }
+    ]
+  }
+]
